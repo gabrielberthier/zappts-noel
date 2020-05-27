2fea9a44a9bdb2800c008d150972e6c2ef8fe19b
Gabriel Berthier <gnberthier@gmail.com>
2020-05-27 16:32:49 -0300

    refactor: move LogDecorator implementation to factory method

--- src/presentation/decorators/log-controller-decorator.spec.ts
+++ src/presentation/decorators/log-controller-decorator.spec.ts
@@ -1,22 +1,38 @@
 import { LogControllerDecorator } from './log-controller-decorator'
 import { HttpRequest, Controller, HttpResponse } from '../protocols'
 
-class ControllerStub implements Controller {
-  async handle (request: HttpRequest): Promise<HttpResponse> {
-    const httpResponse: HttpResponse = {
-      statusCode: 200,
-      body: {
-        name: 'Rony'
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (request: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Rony'
+        }
       }
+      return await new Promise(resolve => resolve(httpResponse))
     }
-    return await new Promise(resolve => resolve(httpResponse))
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut, controllerStub
   }
 }
 
 describe('Log Controller Testing', () => {
+  const { controllerStub, sut } = makeSut()
   it('Assure controller handler is called within decorator', async () => {
-    const controllerStub = new ControllerStub()
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest: HttpRequest = {
       body: {
         name: 'any name',

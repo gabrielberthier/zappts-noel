033345ca2c15f80a7a75ab1422584018f291c513
Gabriel Berthier <gnberthier@gmail.com>
2020-06-19 16:34:11 -0300

    feat: ensure auth-middleware returns 403 when no x-access-token is provided

--- src/main/routes/survey/survey.routes.test.ts
+++ src/main/routes/survey/survey.routes.test.ts
@@ -20,7 +20,7 @@ describe('Survey routes', () => {
     await surveyCollection.deleteMany({})
   })
 
-  it('Should return 204 on post to /surveys', async () => {
+  it('Should return 403 on post to /surveys wothout access token', async () => {
     const password = await hash('123456', 12)
     const addSurveyModel = {
       question: 'any_question',
@@ -41,5 +41,28 @@ describe('Survey routes', () => {
       password
     })
     await request(app).post('/api/surveys').send(addSurveyModel).expect(403)
+  })
+
+  it('Should return 204 on post to /surveys', async () => {
+    const password = await hash('123456', 12)
+    const addSurveyModel = {
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          image: 'any_image2',
+          answer: 'any_answer2'
+        }
+      ]
+    }
+    await surveyCollection.insertOne({
+      name: 'El Gabus',
+      email: 'gabsthier@gmail.com',
+      password
+    })
+    await request(app).post('/api/surveys').send(addSurveyModel).expect(204)
   })
 })

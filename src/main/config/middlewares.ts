24bae3923b00d925f63c90d45f5d5a96907d5e60
Gabriel Berthier <gnberthier@gmail.com>
2021-06-07 00:00:27 -0300

    docs: attempt to use swagger docs

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,15 @@
 import { Express } from 'express'
 import { bodyParser, cors, contentType } from '../middlewares'
+import { serve, setup } from 'swagger-ui-express'
+import { readFileSync } from 'fs'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(contentType)
   app.use(cors)
+  const swaggerFile = (process.cwd() + '/swagger/swagger.json')
+  const swaggerData = readFileSync(swaggerFile, 'utf8')
+
+  const swaggerDocument = JSON.parse(swaggerData)
+  app.use('/api/docs', serve, setup(swaggerDocument))
 }
